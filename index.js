bff25b1e924ca4f58e68089dc2f987aa25f06272
manoj <manojchandanada2001@gmail.com>
2024-04-25 10:17:47 +0000

    solution

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div className='backgroundu'>
+    <div className='total'>
+      <h1 className='heading'>Super Over League</h1>
+      <div className='imagilu'>
+        <img
+          src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+          className='size'
+        />
+        <img
+          src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+          className='size'
+        />
+      </div>
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
